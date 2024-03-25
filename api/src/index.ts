4eb67945a2412ef92cceaacb03c2f0b03d30d1f7
Mahir1902 <mahirhaqe7@gmail.com>
2024-03-25 14:01:23 +1100

    fixed small error

--- api/src/index.ts
+++ api/src/index.ts
@@ -20,7 +20,7 @@ app.use('/api',rootRouter)
 
 app.use(express.static(path.join(__dirname, '/client/dist')))
 
-app.get('*', (req: Request, res: Response) {
+app.get('*', (req: Request, res: Response) => {
   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'))
 })
 
